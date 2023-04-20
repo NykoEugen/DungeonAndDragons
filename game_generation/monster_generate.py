fbfa711ed6d248272adcf28a6235580cf4ec797b
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2023-04-20 23:02:50 +0300

    1

--- game_generation/monster_generate.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-class Monster:
-    def unpack_json(self):
-        with open("rooms.json", "r+") as file:
-            data = file.read()
-            dict_data = json.loads(data)
-        return dict_data
